7bc929cdb885b32e3824f1fbf8a114120222bdd2
ashwani-infobeans <ashwani.sharma@infobeans.com>
2016-08-02 16:52:04 +0530

    added green border

--- index.php
+++ index.php
@@ -5,4 +5,4 @@ echo "Hello World. This is my first repository on git.";
 
 ?>
 <br/><br/>
-<input type="text" name="test" value="This is input box" style="width: 16%;"/>
+<input type="text" name="test" value="This is input box" style="width: 16%; border: 1px solid green;"/>
