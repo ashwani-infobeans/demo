b5935499b98ff77ae5db4be8a2601673d479a8eb
ashwani-infobeans <ashwani.sharma@infobeans.com>
2016-08-02 16:46:59 +0530

    added submit btn

--- index.php
+++ index.php
@@ -6,3 +6,4 @@ echo "Hello World. This is my first repository on git.";
 ?>
 <br/><br/>
 <input type="text" name="test" value="This is input box" style="width: 16%;"/>
+<input type="submit" name="submit" value="Submit" />
